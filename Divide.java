22c7ff277292623a3c8d09e1c5851848e33979c5
meghanapriyanka <52244374+meghanapriyanka@users.noreply.github.com>
2019-07-12 15:00:43 +0530

    Add files via upload

---
+++ Divide.java
@@ -0,0 +1,17 @@
+package fundamentals;
+
+public class Divide {
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=120;i++) {
+		if(i%2==0&&i%3==0&&i%5==0)
+		{
+			System.out.println(i);
+		
+		
+		}
+		}
+
+	}
+
+}
